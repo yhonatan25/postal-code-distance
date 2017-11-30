b58a8673f23739daef41457f4bce86e02ea52bd1
Jonatan_Mendoza <yonatanimm@gmail.com>
2017-11-29 22:15:19 -0600

    Ensures distance requests does not allow empty or null postal codes.

--- src/main/java/org/yhonatan/practice/postal/code/distance/core/DistanceRequest.java
+++ src/main/java/org/yhonatan/practice/postal/code/distance/core/DistanceRequest.java
@@ -1,5 +1,7 @@
 package org.yhonatan.practice.postal.code.distance.core;
 
+import org.springframework.util.Assert;
+
 public class DistanceRequest {
 
     private final String initialPostalCode;
@@ -20,6 +22,8 @@ public class DistanceRequest {
     }
 
     public static DistanceRequest aDistanceRequest(final String initialPostalCode, final String finalPostalCode) {
+        Assert.hasText(initialPostalCode, "Initial postal code must have text.");
+        Assert.hasText(finalPostalCode, "Final postal code must have text.");
         return new DistanceRequest(initialPostalCode, finalPostalCode);
     }
 }
---
+++ src/test/java/org/yhonatan/practice/postal/code/distance/core/DistanceRequestTest.java
@@ -0,0 +1,30 @@
+package org.yhonatan.practice.postal.code.distance.core;
+
+import org.junit.Test;
+
+import static org.yhonatan.practice.postal.code.distance.core.DistanceRequest.aDistanceRequest;
+
+public class DistanceRequestTest {
+
+    private static final String POSTAL_CODE = "AB10 1XG";
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testCreateADistanceRequestWhenReceivingNullInitialPostalCode() {
+        aDistanceRequest(null, POSTAL_CODE);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testCreateADistanceRequestWhenReceivingEmptyInitialPostalCode() {
+        aDistanceRequest(null, POSTAL_CODE);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testCreateADistanceRequestWhenReceivingNullFinalPostalCode() {
+        aDistanceRequest(POSTAL_CODE, null);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testCreateADistanceRequestWhenReceivingEmptyFinalPostalCode() {
+        aDistanceRequest(POSTAL_CODE, null);
+    }
+}
